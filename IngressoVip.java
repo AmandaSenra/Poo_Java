294ce3ed1df2a89008a275f929b15c37eba350d7
AmandaSenra <senraamanda16@gmail.com>
2023-03-24 17:49:01 -0300

    Create IngressoVip.java

---
+++ IngressoVip.java
@@ -0,0 +1,12 @@
+public class IngressoVip extends Ingresso{
+    double valorAdicional;
+
+    public double calcularValor(){
+        return this.valor + this.valorAdicional;
+    }
+
+    @Override
+    public String toString(){
+        return "IngressoVip\n" + this.valorAdicional;
+    }
+}
